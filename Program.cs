56235e3f0d0a0a7de002c1b76f990154f9463cc8
HectorGonzalezGonzalez <37567557+HectorGonzalezGonzalez@users.noreply.github.com>
2020-08-09 17:41:54 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace thread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(Ejemplo);
+            t.Start("Z");
+
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write("A");
+            }
+
+            
+            Console.Read();
+        }
+
+    
+
+        public static void Ejemplo(object o)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write(o);
+            }
+        }
+    }
+}
